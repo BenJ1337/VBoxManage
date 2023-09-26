df1263cd0e5b8661a36d6d2e61dc91a55c60f5e0
Benjamin Hacker <email@benjamin-hacker.de>
2023-09-26 16:11:56 +0200

    update

--- vars.bat
+++ vars.bat
@@ -1,7 +1,22 @@
-SET VM_NAME=vm-ubuntu-server-22-04-1
+REM -- Beginning of main script --
 
-SET DISC_PATH=./hdd/
+call :loadVarsFromIni env.ini
+ECHO %array[general][MEDIUM_PATH]%
 
-SET ISO_PATH=./iso/ubuntu-22.04.1-live-server-amd64.iso
+exit /b 0 REM End of main script
 
-SET HOST_ONLY="VirtualBox Host-Only Ethernet Adapter #2"
+REM -- Methods --
+    :loadVarsFromIni <filename>
+    for /f "usebackq delims=" %%a in ("%~1") do (
+        set ln=%%a
+        if "x !ln:~0,1!"=="x [" (
+            set currarea=!ln:~1,-1!
+        ) else (
+            for /f "tokens=1,2 delims==" %%b in ("!ln!") do (
+                set currkey=%%b
+                set currval=%%c
+                set array[!currarea!][!currkey!]=!currval!
+            )
+        )
+    )
+    exit /b 0
